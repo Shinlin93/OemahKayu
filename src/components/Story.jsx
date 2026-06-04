ee3db5aa8acb49b6b8506a6d91d5f70bbc383e63
Shinlin93 <ichsanfaisal04@gmail.com>
2026-06-04 15:45:59 +0700

    Update Story.jsx

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,4 +1,4 @@
-import video1 from "../assets/video1.mp4";
+import Video1 from "../assets/Video1.mp4";
 
 export default function Story() {
   return (
@@ -19,7 +19,7 @@ export default function Story() {
           playsInline
           className="absolute inset-0 w-full h-full object-cover"
         >
-          <source src={video1} type="video/mp4" />
+          <source src={Video1} type="video/mp4" />
         </video>
 
         {/* Overlay */}
